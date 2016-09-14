8522b5a47a31943ae7df9652465abff6a8459a16
enuggetry <github@meyao.net>
2016-09-13 23:59:50 -0700

    global model/controller

---
+++ api/controllers/JbGlobalController.js
@@ -0,0 +1,11 @@
+/**
+ * JglobalController
+ *
+ * @description :: Server-side logic for managing jglobals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
