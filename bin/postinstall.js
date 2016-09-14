fa21f421e6fd8af9c937111acfb6882fba5d7313
enuggetry <github@meyao.net>
2016-09-14 00:03:24 -0700

    implement post install to copy template.json file to jblast result directory.

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 
-console.log("jbserver post install thingy");
+console.log("jbserver post install...");
 
-var g = sails.config.globals;
+var fs = require("fs");
+var g = require("../config/globals.js").globals;
+
+//console.log(g);
 
 var blastPath = g.jbrowse.jbrowsePath + g.jbrowse.dataSet[0].dataPath + g.jbrowse.jblast.blastResultPath;
-theBlastFile = blastPath+'/'+theBlastFile; 
-console.log("theBlastFile",theBlastFile);
+blastPath += '/';
+
+console.log('blastPath',blastPath);
 
 // if direcgtory doesn't exist, create it
 if (!fs.existsSync(blastPath)){
     fs.mkdirSync(blastPath);
 }  
+
+// copy template file
+var tFile = "inMemTemplate.json";
+
+console.log('Copying template file', tFile);
+
+fs.createReadStream('./bin/'+tFile).pipe(fs.createWriteStream(blastPath+tFile));
