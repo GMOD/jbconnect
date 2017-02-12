cf251af720c77af274678fdca035f42d7b73d388
enuggetry <github@meyao.net>
2017-02-11 23:19:53 -0800

    add jquery-ui copy update jquery copy

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -6,11 +6,24 @@ console.log("jbserver setup");
 var g = require("../config/globals.js").globals;
 var fs = require("fs-extra");
 
-
 fs.copy(__dirname + '/../node_modules/bootstrap/dist',__dirname + '/../assets/bootstrap',function(err) {
-    console.log("copied bootstrap.");
+    if (err) {
+        console.log("error copying bootstrap",err);
+        return;
+    }
+    console.log("copied assets/bootstrap.");
 });
-fs.copy(__dirname + '/../node_modules/jquery/dist',__dirname + '/../assets/jquery',function(err) {
-    console.log("copied jquery.");
+fs.copy(__dirname + '/../node_modules/jquery-ui-dist',__dirname + '/../assets/jquery-ui',function(err) {
+    if (err) {
+        console.log("error copying jquery-ui",err);
+        return;
+    }
+    console.log("copied assets/jquery-ui.");
+    fs.copy(__dirname + '/../assets/jquery-ui/external/jquery',__dirname + '/../assets/jquery',function(err) {
+        if (err) {
+            console.log("error copying jquery",err);
+            return;
+        }
+        console.log("copied assets/jquery.");
+    });
 });
-
