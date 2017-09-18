d3ef6f28d794ce20ea87d7b189318911200c752d
enuggetry <github@meyao.net>
2017-09-17 21:32:17 -0700

    add postinatall.js and postsetup.js

---
+++ bin/postinstall.js
@@ -0,0 +1,9 @@
+var shelljs = require('shelljs');
+
+// run blast_getBlast.js
+//shelljs.exec('node blast_getBlast.js');
+
+console.log("Download and install blast database: node blast_getBlast.js");
+console.log("install existing blast database: node blast_setPath.js <path of existing db>");
+
+
