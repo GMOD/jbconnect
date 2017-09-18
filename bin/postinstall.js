f615fa0940fe27f78ea3ed2ff2ee3b6c54e576a0
enuggetry <github@meyao.net>
2017-09-17 21:37:17 -0700

    remove special instructions, as they don't apply

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -3,7 +3,7 @@ var shelljs = require('shelljs');
 // run blast_getBlast.js
 //shelljs.exec('node blast_getBlast.js');
 
-console.log("Download and install blast database: node blast_getBlast.js");
-console.log("install existing blast database: node blast_setPath.js <path of existing db>");
+//console.log("Download and install blast database: node blast_getBlast.js");
+//console.log("install existing blast database: node blast_setPath.js <path of existing db>");
 
 
