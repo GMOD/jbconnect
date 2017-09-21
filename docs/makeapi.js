4af273187da13c01b03dbf30519d1b36b1e6c230
enuggetry <github@meyao.net>
2017-09-20 19:50:48 -0700

    add code to ignore files with ##excludedoc

--- docs/makeapi.js
+++ docs/makeapi.js
@@ -10,25 +10,23 @@ var outfile = './api.rst';
 
 var ignore = ['conf.py','index.rst','readme.md'];
 
-// remove file
+// remove api.rst file
 if (fs.existsSync(outfile))
     fs.unlinkSync(outfile);
 
-console.log('generating api.rst');
+console.log('generating api.rst...');
 
 // main title
 fs.appendFileSync(outfile,'***\nAPI\n***\n\n');
 
-
+// scan through each file in directory
 fs.readdirSync(dirpath).forEach(file => {
 
     if (ignore.indexOf(file) == -1) {  // if file is not in ignore list
-        
-        console.log("appending --",file);
 
         var array = fs.readFileSync(dirpath+file).toString().split("\n");
         
-        processFile(array);
+        processFile(array,file);
         //process.exit(1);
     }
 });
@@ -37,13 +35,22 @@ fs.readdirSync(dirpath).forEach(file => {
  * @param {type} array
  * @returns {undefined}
  */
-function processFile(lines) {
+function processFile(lines,file) {
     // remove the first line - above title
     lines.splice(0,1);
     
     // change the title level of the first line 
     lines[1] = replaceall('=','*',lines[1]);
     
+    // skip file if "##excludedoc" is found
+    for (i in lines) {
+        if (lines[i].indexOf("##excludedoc") > -1) {
+            console.log("..ignoring --",file);
+            return;
+        }
+    }
+    
+    console.log("appending --",file);
     for(i in lines) {
         // remove "Children" section
         if (lines[i].indexOf("Children") > -1) {
