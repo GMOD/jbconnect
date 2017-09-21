f51ac2ea2ba82537e52a652119b73211e09aeeb1
enuggetry <github@meyao.net>
2017-09-20 19:50:08 -0700

    add delete genapi-rst/*.rst files

--- docs/gendocs_run.js
+++ docs/gendocs_run.js
@@ -9,7 +9,13 @@ var shelljs = require('shelljs');
 var jsdoc = './node_modules/jsdoc/jsdoc.js -t node_modules/jsdoc-sphinx/template/ -d ./docs/genapi-rst ./api/**/*.js';
 var makeapi = 'node makeapi.js';
 
+// remove genapi-rst/*.rst files
+shelljs.exec('sudo rm -f ./genapi-rst/*.rst',{silent:false});
+
+// run jsdoc
 shelljs.exec(jsdoc,{silent:false});
+
+// run makapi
 process.chdir('./docs');
 //console.log('cwd',process.cwd());
 shelljs.exec(makeapi,{silent:false});
