05e48b38bd18fb2734bfba0d69c285dd49a88136
enuggetry <github@meyao.net>
2017-09-21 17:48:21 -0700

    fix delete directory

--- docs/gendocs_run.js
+++ docs/gendocs_run.js
@@ -10,7 +10,7 @@ var jsdoc = './node_modules/jsdoc/jsdoc.js -t node_modules/jsdoc-sphinx/template
 var makeapi = 'node makeapi.js';
 
 // remove genapi-rst/*.rst files
-shelljs.exec('sudo rm -f ./genapi-rst/*.rst',{silent:false});
+shelljs.exec('rm -f ./docs/genapi-rst/*.rst',{silent:false});
 
 // run jsdoc
 shelljs.exec(jsdoc,{silent:false});
