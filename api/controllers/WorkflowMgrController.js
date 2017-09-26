4e2f4535e00f9ad3215a6185efd0b53bf0be2879
enuggetry <github@meyao.net>
2017-09-26 01:45:23 -0700

    add workflow manager model / controller

---
+++ api/controllers/WorkflowMgrController.js
@@ -0,0 +1,16 @@
+/**
+ * @module
+ * @ignore
+ * @description
+ * todo: document 
+ */
+
+var jblastProc = require('../services/jblastProc');
+
+module.exports = {
+    hello: function (req, res, next) {
+        return res.send('WorkflowMgr hello');
+    },
+    getWorkflows: jblastProc.getWorkflows
+};
+
