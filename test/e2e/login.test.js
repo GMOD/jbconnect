ef4d46006fba8d94420c3e36030e7ce0693a40af
enuggetry <github@meyao.net>
2017-10-19 18:49:15 -0700

    clarify end-2-end test describe

--- test/e2e/login.test.js
+++ test/e2e/login.test.js
@@ -13,55 +13,59 @@ var client = nightwatch.initClient(nw_conf);
 var browser = client.api();
 console.log('login.test.js','nightwatchTest',nightwatchTest);
 
-describe('JBClient Login', function() {
-/*
-    var nightwatchConf = require('../nightwatch.conf.js');
-    var nw_conf = Object.assign(
-        nightwatchConf.test_settings[nightwatchTest]
-    );
-    var client = nightwatch.initClient(nw_conf);
+describe('End-2-End Tests -------------------------------------------------------', function() {
 
-    var browser = client.api();
-*/
-//    browser.globals = require('../browserGlobals.js');
-    
-    this.timeout(99999999);
 
-    before(function() {
-        browser.perform(function() {
-            console.log('beforeAll');
+    describe('JBClient Login', function() {
+    /*
+        var nightwatchConf = require('../nightwatch.conf.js');
+        var nw_conf = Object.assign(
+            nightwatchConf.test_settings[nightwatchTest]
+        );
+        var client = nightwatch.initClient(nw_conf);
+
+        var browser = client.api();
+    */
+    //    browser.globals = require('../browserGlobals.js');
+
+        this.timeout(99999999);
+
+        before(function() {
+            browser.perform(function() {
+                console.log('beforeAll');
+            });
         });
-    });
 
-    beforeEach(function(done) {
-        browser.perform(function() {
-            console.log('beforeEach');
+        beforeEach(function(done) {
+            browser.perform(function() {
+                console.log('beforeEach');
+            });
+            client.start(done);
         });
-        client.start(done);
-    });
 
-    it('Test Login', function (done) {
-        browser
-            .url('http://localhost:1337/login')
-            .waitForElementVisible('body', 5000)
-            .assert.title('New Sails App')
-            .waitForElementVisible('body', 1000)
-            .assert.visible('h4.modal-title')
-            .assert.containsText('h4.modal-title', 'JBrowse Login', 'Checking login box title');
+        it('Test Login', function (done) {
+            browser
+                .url('http://localhost:1337/login')
+                .waitForElementVisible('body', 5000)
+                .assert.title('New Sails App')
+                .waitForElementVisible('body', 1000)
+                .assert.visible('h4.modal-title')
+                .assert.containsText('h4.modal-title', 'JBrowse Login', 'Checking login box title');
 
-        client.start(done);
-    });
-
-    afterEach(function() {
-        browser.perform(function() {
-            console.log('afterEach');
+            client.start(done);
         });
-    });
 
-    after(function(done) {
-        browser.end(function() {
-            console.log('afterAll');
+        afterEach(function() {
+            browser.perform(function() {
+                console.log('afterEach');
+            });
         });
-        client.start(done);
+
+        after(function(done) {
+            browser.end(function() {
+                console.log('afterAll');
+            });
+            client.start(done);
+        });
     });
 });
