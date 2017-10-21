d68027d90b906787514a073d8167ca827c5ee5a2
enuggetry <github@meyao.net>
2017-10-21 13:31:23 -0700

    Implement installed service framework

--- api/services/serviceProc.js
+++ api/services/serviceProc.js
@@ -1,25 +1,54 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 
 
 module.exports = {
     
     /*
-     * associative array of services, keyed by service name
-     * service element:
-     * {
-     *    
-     * }
+     * Associative array of services, keyed by service name.
+     * 
+     * addService() creates the items in this list, providing quick access to
+     * service handlers keyed by service name.
+     * 
+     * Each item:  
+     *      <service name>: <service handler>
+     *      
+     * for example:
+     *      'galaxy_blast' : <func ptr to galaxyService in jblast>    
      */
     services: {},
+    
+    /*
+     * Associative array of service names keyed by function name.
+     * 
+     * addService() creates the items in this list, providing quick access to
+     * services given a command name.
+     * 
+     * If multiple services have the same command name, the earlier registrant's
+     * commands will tend to dominate.  Then, the long command form should
+     * be used to address a specific service's commands.
+     * 
+     * Short form route:
+     *      /service/exec/workflow_submit
+     *      
+     * Long form route:
+     *      /service/exec/workflow_submit:galaxy_blast
+     * 
+     * 
+     * 
+     * Each array item:
+     *      <command name> : <service name>
+     * 
+     * for example:
+     *      'workflow_submit' : 'galaxy_blast'
+     */
+    cmdMap:{},
     
     init: function(cb) {
         sails.log.debug(">>> Service.init()");
         
         // delete database entries
+        /*
         Service.destroy({},function (err,theDeleted){
             if (err) {
                 cb(err);
@@ -28,30 +57,88 @@ module.exports = {
             sails.log.debug('>>> Service records deleted, if there were any.',theDeleted);
             cb();
         });
+        */
+        // we must do a deferred action in init, so since we commented out the destroy...
+        Service.find({},function(err,found) {
+           cb(); 
+        });
     },
     
     addService: function(service,cb) {
         
         var handler = service.handler;
-        delete service[service.name];
+        //delete service[service.name];
         
         sails.log.debug('addService',service);
         
-        Service.create( service,function(err, theService){
-            
-            sails.log.debug('post find create',err,theService);
-            sails.log.info('Add Service:',theService.name,theService.type,'(',theService.module,')');
-            
-            serviceProc.services[theService.name] = handler;
-            cb(err);
+        if (typeof service.name === 'undefined') {
+            sails.log('addService - no service name');
+            return cb('addService - no service name');
+        } 
+        if (typeof service.handler === 'undefined') {
+            sails.log('addService - no handler defined',service.handler);
+            return cb('addService - no handler defined');
+        }
+        
+        Service.updateOrCreate({name:service.name},service).then(function(record) {
+            sails.log('service added',service.name);
+            _addService(handler,service.name);
+            return cb();
+        }).catch(function() {
+            sails.log('error adding service',service.name);
+            return cb(err);
         });
-    },
-    
-    enumServices: function (req, res) {
-        sails.log.debug("enumServices()");
+        
+        function _addService(svc) {
+            serviceProc.services[service.name] = service.handler;
+
+            for(var cmd in handler.fmap) {
+                if (typeof serviceProc.cmdMap[cmd] === 'undefined')
+                    serviceProc.cmdMap[cmd] = service.name;
+            }
+        }
     },
     
     execute: function (req, res) {
+
+        var serviceName = "***";
+        
+        //sails.log('service.execute',req.route);
+        sails.log('params',req.params,'body',req.body);
+        
+        // given the route /service/exec/mycmd, id will be 'mycmd', the command
+        var cmdName = req.params['id'];
+        
+        // if it contains a ':', it is a long form command
+        if (cmdName.indexOf(':') !== -1) {
+            var x = cmdName.split(':');
+            cmdName = x[0];
+            serviceName = x[1];
+        }
+        else {
+            // attempt to get service name from map
+            serviceName = serviceProc.cmdMap[cmdName];
+        }
+        sails.log("service %s cmd %s",serviceName,cmdName, serviceProc.cmdMap);
+        
+        // does service exist or is it registered
+        if (typeof this.services[serviceName] === 'undefined') {
+            sails.log.error('service does not exist:',serviceName);
+            return res.forbidden();
+        }
+        // determine type of request GET/POST, etc.  Reject invalid
+        var method = req.method;
+        method = method.toLowerCase();
+        if (method !== this.services[serviceName].fmap[cmdName]) {
+            sails.log.error('forbidden request method:',req.method,method,this.services[serviceName].fmap[cmdName]);
+            return res.forbidden();
+        }
+        // is the command implemented
+        if (typeof this.services[serviceName].fmap[cmdName] === 'undefined' || typeof this.services[serviceName][cmdName] !== 'function') {
+            sails.log.error('invalid command:',cmdName);
+            return res.forbidden();
+        }
+        this.services[serviceName][cmdName](req,res);
         
     }
 };
