87072c4c654c4977236e96445def487eca404d83
enuggetry <github@meyao.net>
2017-11-28 09:51:17 -0800

    expose addTrack, modifyTrack, removeTrack as rest interfaces

--- api/controllers/TrackController.js
+++ api/controllers/TrackController.js
@@ -10,6 +10,49 @@
  */
 
 module.exports = {
-	
+    /**
+     * 
+     * @param {object} req
+     * @param {object} res
+     * @returns {unresolved}
+     */
+    addTrack: function(req,res) {
+        var params = req.allParams();
+        var track = params.track;
+        if (req.method === 'POST') {
+            Track.addTrack(track,function(err,created) {
+                if (err) return res.serverError({err:err,track:track});
+                return res.ok(created);
+            });
+        } 
+        else 
+            return res.forbidden('requires POST');
+    },
+    modifyTrack: function(req,res) {
+        var params = req.allParams();
+        var track = params.track;
+        if (req.method === 'POST') {
+            Track.modifyTrack(track,function(err,modified) {
+                if (err) return res.serverError({err:err,track:track});
+                return res.ok(modified);
+            });
+        } 
+        else 
+            return res.forbidden('requires POST');
+        
+    },
+    removeTrack: function(req,res) {
+        var params = req.allParams();
+        var id = params.trackId;
+        if (req.method === 'POST') {
+            Track.destroyTrack(id,function(err) {
+                if (err) return res.serverError({err:err});
+                return res.ok();
+            });
+        } 
+        else 
+            return res.forbidden('requires POST');
+        
+    }
 };
 
