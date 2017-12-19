e5f86bc8f72c4423b6e16c9da1564e3e8a92d8e4
enuggetry <github@meyao.net>
2017-12-18 19:59:21 -0800

    implement SetAdmin and SetPassword functions, which are generally accessible from the sails console.  #46.

--- api/models/User.js
+++ api/models/User.js
@@ -13,6 +13,7 @@ var User = {
     attributes: {
       username  : { type: 'string', unique: true },
       email     : { type: 'email',  unique: true },
+      admin     : { type: 'boolean', defaultsTo: false},
       passports : { collection: 'Passport', via: 'user' }
     },
     /**
@@ -27,6 +28,73 @@ var User = {
            return cb(err);
         });
     },
+    /*
+     * Set password for a given user
+     * 
+     * sails> User.SetPassword('juser','password',console.log);
+     * undefined
+     * null 'Password for juser changed.'
+
+     * @param {string} user - username
+     * @param {string} pass - new password
+     * @param {function} cb - callback function
+     * @returns {undefined}
+     */
+    SetPassword: function(user,pass,cb) {
+    
+        this.findOne({username:user}).exec(function(err,ufound) {
+            if (err) {
+                if (typeof cb === 'function' ) return cb(err);
+                else return;
+            }
+            //console.log("found user",ufound);
+            
+            Passport.update({user:ufound.id},{password:pass}).exec(function(err,updated){
+                if (err) {
+                    if (typeof cb === 'function' ) return cb(err);
+                    else return;
+                }
+                var msg = 'Password for '+user+' changed.';
+                //console.log(msg);
+                
+                if (typeof cb === 'function' ) return cb(null,msg);
+                else return;
+            });
+            
+        });
+    },
+    /*
+     * Set the admin flag for a user 
+     * 
+     * Example:
+     * sails> User.SetAdmin('juser',true,console.log);
+     * undefined
+     * null 'Set admin flag for juser to true'
+     * 
+     * @param {string} user - username
+     * @param {boolean} adminflag - true for false
+     * @param {function} cb - callback function
+     */
+    SetAdmin: function(user,adminflag,cb) {
+
+        if ( typeof adminflag !== 'boolean') {
+            if (typeof cb === 'function' ) return cb('Admin flag must be boolean.');
+            else return;
+        }
+        
+        this.update({username:user},{admin: adminflag}).exec(function(err,updated) {
+            if (err) {
+                if (typeof cb === 'function' ) return cb(err);
+                else return;
+            }
+            var msg = 'Set admin flag for '+user+' to '+updated[0].admin;
+            //console.log(msg);
+
+            if (typeof cb === 'function' ) return cb(null,msg);
+            else return;
+            
+        });
+    }
 };
 
 module.exports = User;
