312c304a94635e7bc24b8e9e57d8cb0f840ff6ac
enuggetry <github@meyao.net>
2018-03-02 04:19:39 -0800

    implement - don't copy config.js if it already exists.

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -6,6 +6,12 @@ var jbutillib = require('../api/services/jbutillib.js');
 // install default database, don't overwrite if it exists.  Use jbutil --dbreset to reset to default.
 jbutillib.install_database();
 
+// don't copy config.js if it already exists.
+let configjs = approot+"/config.js";
+if (!fs.existsSync(configjs)) {
+    shelljs.cp(approot+'/bin/config.js',configjs);
+}
+
 // run jbrowse jb_setup.js to setup sample data
 console.log("Installing JBrowse sample data...");
 var cmd = './jb_setup.js';
