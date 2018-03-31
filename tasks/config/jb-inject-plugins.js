1d69e262125e1026d01e44afc7fc2d6e7fa2167d
enuggetry <github@meyao.net>
2018-03-30 18:57:58 -0700

    Upon post plugin injection, run `npm run build` for JBrowse, to execute webpack build to properly integrate plugins.

--- tasks/config/jb-inject-plugins.js
+++ tasks/config/jb-inject-plugins.js
@@ -1,12 +1,22 @@
 /* 
  * Inject JBConnect client plugins into JBrowse
  */
+const jblib = require('../../api/services/jbutillib');
+const approot = require('app-root-path'); 
+const sh = require('shelljs');
 
 module.exports = function (grunt) {
     grunt.registerTask('jb-inject-plugins', 'Inject JBConnect client plugins into JBrowse', function() {
         grunt.log.writeln('Inject JBConnect client plugins into JBrowse');
-        let jblib = require('../../api/services/jbutillib');
-        jblib.injectPlugins();
+        let countInstalled = jblib.injectPlugins();
         jblib.setupPlugins();
+        
+        if (countInstalled) {
+            let origcwd = process.cwd();
+            let cfg = jblib.getMergedConfig();
+            process.chdir(cfg.jbrowsePath);
+            sh.exec("npm run build");
+            process.chdir(origcwd);
+        }
     });
 };
