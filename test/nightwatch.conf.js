9847b2ecd43a7c52c4b1dba03962aa541bbfc8ae
enuggetry <github@meyao.net>
2018-04-26 19:17:26 -0700

    update nightwatch

--- test/nightwatch.conf.js
+++ test/nightwatch.conf.js
@@ -1,4 +1,16 @@
+
+const fs = require("fs");
+
+module.exports = ((settings) => {
+  const seleniumServerFileName =
+  fs.readdirSync("node_modules/selenium-standalone/.selenium/selenium-server/");
+  settings.selenium.server_path += seleniumServerFileName;
+  return settings;
+})(require("./nightwatch.json"));
+
+/*
 module.exports = (function(settings) {
   settings.test_workers = false;
   return settings;
 })(require('./nightwatch.json'));
+*/
