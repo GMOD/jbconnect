96f8b91045cf3b1c0694fac104feaa03290628cf
enuggetry <github@meyao.net>
2018-07-12 17:05:31 -0700

    remove unused deepmerge module

--- config/globals.js
+++ config/globals.js
@@ -13,7 +13,6 @@
  * Note: globals defined in jbconnect take precedent over jbconnect-hook-* module globals.js definitions. 
  */
 
-var merge = require('deepmerge');
 var approot = require('app-root-path');
 var jbPath = approot + "/node_modules/@gmod/jbrowse/";
 var util = require(approot+"/api/services/utilFn");
