fd7d9feae60b737f265dfac90a500659cb9fac8f
enuggetry <github@meyao.net>
2018-07-30 01:26:49 -0700

    remove testing code, now covered in integration test.

--- test/unit/controllers/UserController.test.js
+++ test/unit/controllers/UserController.test.js
@@ -1,18 +1,25 @@
-var request = require('supertest');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const server = 'http://localhost:1337';
+const expect = chai.expect;
+const assert = chai.assert;
+//var request = require('supertest');
 
 describe('UserController', function() {
 
-    var url = 'http://localhost:1337';
+    agent = chai.request.agent(server);
 
-    describe('/loginstate before login', function() {
-        
-        it('should return empty object', function (done) {
-            request(sails.hooks.http.app)
-              .get('/login')
-              .set('Accept', 'application/json')
-              .expect(200)
-              .expect({},done);
-        });
-    });
+//    describe('/loginstate before login', function() {
+//        
+//        it('should return empty object', function (done) {
+//            agent(sails.hooks.http.app)
+//              .get('/login')
+//              .set('Accept', 'application/json')
+//              .expect(200)
+//              .expect({},done);
+//        });
+//    });
 
 });
