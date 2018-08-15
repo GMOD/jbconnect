02e62dd51f71b90765fa2d67549fa24660dbdfca
enuggetry <eynugget@meyao.net>
2018-08-14 20:42:31 -0400

    add some clarifying comments

--- tasks/config/jb-inject-plugins.js
+++ tasks/config/jb-inject-plugins.js
@@ -8,13 +8,20 @@ const sh = require('shelljs');
 module.exports = function (grunt) {
     grunt.registerTask('jb-inject-plugins', 'Inject JBConnect client plugins into JBrowse', function() {
         grunt.log.writeln('Inject JBConnect client plugins into JBrowse');
+
+        // plugins are added to the JBrowse plugins directory by symlink
         let countInstalled = jblib.injectPlugins();
+
+        // add plugins into trackList.json
         jblib.setupPlugins();
-        
+
         if (countInstalled) {
             let origcwd = process.cwd();
             let cfg = jblib.getMergedConfig();
             process.chdir(cfg.jbrowsePath);
+
+            // todo: future enhancement - bypass the build if nothing has changed.
+
             sh.exec("npm run build");
             process.chdir(origcwd);
         }
