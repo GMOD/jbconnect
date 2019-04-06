1332c25bb30720253f3394697cbc26fd92a2b8d4
enuggetry <ericiam@berkeley.edu>
2019-04-06 02:44:51 -0400

    change /democleanup to a POST

--- api/controllers/UtilController.js
+++ api/controllers/UtilController.js
@@ -11,138 +11,144 @@ module.exports = {
 	 * req/res are express-based.
 	 */
 	demoCleanup: function(req, res) {
-		sails.log.info("********** demo cleanup *********************");
-	
-		let nJobs = 0;
-		let nTracks = 0;
-		let simulate = true;  // for debug.  if true, won't actually delete anything.
+		if (req.method === 'POST') {
 
-		if (simulate) sails.log.info("simulate");
-
-		_deleteJobs(function(err) {
-			_deleteTracks(function(err) {
-				return res.ok({tracks:nTracks,jobs:nJobs});
-			});
-		});
+			sails.log.info("********** demo cleanup *********************");
 		
-		function _deleteJobs(next) {
-			sails.log.info("deleting jobs");
-			Job.find({})
-			.catch(function(err) {
-				console.log("Job.find failed");
-			})
-			.then(function(records) {
-				if (records && records.length > 0) {
-					
-					let deleteRecs = [];
-					
-					for(var i in records ) {
-						if (!_.isUndefined(records[i].data.keep)) continue;
-						
-						deleteRecs.push(records[i]);
-					}
-					if (deleteRecs.length) {
-					
-						//deleteRecs.forEach(function(rec) {
-						//	sails.log.info("will remove job",rec.id,rec.data.name);
-						//});
+			let nJobs = 0;
+			let nTracks = 0;
+			let simulate = true;  // for debug.  if true, won't actually delete anything.
 
-						let count = deleteRecs.length - 7;
-						deleteRecs2 = [];
-						for(let i=0;i < count;i++) deleteRecs2.push(deleteRecs[i]);
-						
-						deleteRecs2.forEach(function(rec) {
-							sails.log.info("selected to remove",rec.id,rec.data.name);
-						});
+			if (simulate) sails.log.info("simulate");
 
-						deleteRecs = deleteRecs2;
+			_deleteJobs(function(err) {
+				_deleteTracks(function(err) {
+					return res.ok({tracks:nTracks,jobs:nJobs});
+				});
+			});
+			
+			function _deleteJobs(next) {
+				sails.log.info("deleting jobs");
+				Job.find({})
+				.catch(function(err) {
+					console.log("Job.find failed");
+				})
+				.then(function(records) {
+					if (records && records.length > 0) {
 						
-						if (!simulate) {
-							async.eachLimit(deleteRecs,1,
-								function(rec,cb){
-									Job.Remove(rec,function(err,id) {
-										if (err) {
-											sails.log.error("failed to remove",rec.id,rec.data.name,err);
-											return cb(err);
-										}
-										sails.log.info("removed job",rec.id,rec.data.name);
-										return cb();
-									});
-								},
-								function(err){
-									// all done
-									return next();
-								}
-							);
+						let deleteRecs = [];
+						
+						for(var i in records ) {
+							if (!_.isUndefined(records[i].data.keep)) continue;
+							
+							deleteRecs.push(records[i]);
 						}
-						else 
+						if (deleteRecs.length) {
+						
+							//deleteRecs.forEach(function(rec) {
+							//	sails.log.info("will remove job",rec.id,rec.data.name);
+							//});
+
+							let count = deleteRecs.length - 7;
+							deleteRecs2 = [];
+							for(let i=0;i < count;i++) deleteRecs2.push(deleteRecs[i]);
+							
+							deleteRecs2.forEach(function(rec) {
+								sails.log.info("selected to remove",rec.id,rec.data.name);
+							});
+
+							deleteRecs = deleteRecs2;
+							
+							if (!simulate) {
+								async.eachLimit(deleteRecs,1,
+									function(rec,cb){
+										Job.Remove(rec,function(err,id) {
+											if (err) {
+												sails.log.error("failed to remove",rec.id,rec.data.name,err);
+												return cb(err);
+											}
+											sails.log.info("removed job",rec.id,rec.data.name);
+											return cb();
+										});
+									},
+									function(err){
+										// all done
+										return next();
+									}
+								);
+							}
+							else 
+								return next();
+						}
+						else {
+							sails.log.warn("no jobs to delete");
 							return next();
-					}
-					else {
-						sails.log.warn("no jobs to delete");
-						return next();
-					}
-				}
-				else {
-					sails.log.warn("no jobs detected");
-					return next();
-				}
-			});
-		}
-		function _deleteTracks(next) {
-			sails.log.info("deleting tracks");
-			Track.find({dataset:1})
-			.catch(function(err) {
-				return next(err);
-			})
-			.then(function(records) {
-				if (records && records.length > 0) {
-					
-					let deleteRecs = [];
-					
-					for(var i in records ) {
-						if (!_.isUndefined(records[i].trackData.keep)) continue;
-
-                        if ( !_.isUndefined(records[i].trackData.category) && records[i].trackData.category==="JBlast Results" ) {
-                            deleteRecs.push(records[i]);
-                        }
-					}
-					if (deleteRecs.length) {
-						
-						deleteRecs.forEach(function(rec) {
-							sails.log.info("will remove track",rec.lkey);
-						});
-						
-						if (!simulate) {
-							async.eachLimit(deleteRecs,1,
-								function(rec,cb){
-									Track.Remove(rec,function(err,id) {
-										if (err) {
-											sails.log.error("failed to remove",err);
-											return cb(err);
-										}
-										sails.log.info("removed track",rec.lkey);
-										return cb();
-									});
-								},
-								function(err){
-									// all done
-									return next();
-								}
-							);
 						}
-						else return next();
 					}
 					else {
-						sails.log.warn(deleteRecs.length,"tracks. no tracks to delete");
+						sails.log.warn("no jobs detected");
 						return next();
 					}
-				}
-				else {
-					sails.log.warn("no tracks detected");
-					return next();
-				}
-			});
-		}
+				});
+			}
+			function _deleteTracks(next) {
+				sails.log.info("deleting tracks");
+				Track.find({dataset:1})
+				.catch(function(err) {
+					return next(err);
+				})
+				.then(function(records) {
+					if (records && records.length > 0) {
+						
+						let deleteRecs = [];
+						
+						for(var i in records ) {
+							if (!_.isUndefined(records[i].trackData.keep)) continue;
+
+							if ( !_.isUndefined(records[i].trackData.category) && records[i].trackData.category==="JBlast Results" ) {
+								deleteRecs.push(records[i]);
+							}
+						}
+						if (deleteRecs.length) {
+							
+							deleteRecs.forEach(function(rec) {
+								sails.log.info("will remove track",rec.lkey);
+							});
+							
+							if (!simulate) {
+								async.eachLimit(deleteRecs,1,
+									function(rec,cb){
+										Track.Remove(rec,function(err,id) {
+											if (err) {
+												sails.log.error("failed to remove",err);
+												return cb(err);
+											}
+											sails.log.info("removed track",rec.lkey);
+											return cb();
+										});
+									},
+									function(err){
+										// all done
+										return next();
+									}
+								);
+							}
+							else return next();
+						}
+						else {
+							sails.log.warn(deleteRecs.length,"tracks. no tracks to delete");
+							return next();
+						}
+					}
+					else {
+						sails.log.warn("no tracks detected");
+						return next();
+					}
+				});
+			}
+        } 
+        else 
+            return res.forbidden('requires POST');
+
 	}
 };
