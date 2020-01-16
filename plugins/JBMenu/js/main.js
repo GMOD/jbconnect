b43b63fac9af0a1e72820dcc7cfebce6f41daf05
enuggetry <ericiam@berkeley.edu>
2020-01-15 19:40:05 -0800

    add JBMenu plugin

---
+++ plugins/JBMenu/js/main.js
@@ -0,0 +1,259 @@
+/* 
+    JBMenu - Client Menu and other interface integrations
+    Author     : ey
+*/
+
+define([
+        'dojo/_base/declare',
+        'dojo/_base/lang',
+        'dojo/Deferred',
+        'dojo/dom-construct',
+        'dojo/query',
+        'JBrowse/Plugin',
+           'dijit/form/Button',
+           'dijit/Dialog',
+           "dojo/store/Memory",
+           "dijit/form/ComboBox",
+            'dijit/Menu',
+            'dijit/MenuItem',
+           'JBrowse/has'
+       ],
+       function(
+        declare,
+        lang,
+        Deferred,
+        domConstruct,
+        query,
+        JBrowsePlugin,
+        Button, Dialog, Memory, ComboBox,Menu,MenuItem,has
+       ) {
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        console.log("plugin: JBMenu ",args);
+        
+        var thisB = this;
+        var browser = this.browser;
+
+        browser.jbmenuDialog = thisB.Browser_jbmenuDialog;
+
+        browser.jbmenu = {
+            asset: null,
+            browser: browser,
+            focusQueue: [],
+            focusQueueProc: 0,
+            panelDelayTimer: null,
+            bpSizeLimit: args.bpSizeLimit || 0,
+            getWorkflows: this.getWorkflows,
+
+            // check if bpSize > bpSizeLimit, if bpSizeLimit is defined
+            isOversized(bpSize) {
+                let bpSizeLimit = JBrowse.jbmenu.bpSizeLimit;
+
+                if (bpSizeLimit && bpSize > bpSizeLimit) {
+                    // oversize message
+                    alert("Query size is "+bpSize+".  The query size is limited to "+bpSizeLimit+" bp for demonstration purposes.  bpSizeLimit is set in trackList.json.");
+                    return true;
+                }
+                else return false;
+            }
+        };
+
+
+        /*
+         * class override function intercepts
+         */
+        browser.afterMilestone( 'initView', function() {
+            
+         
+            // skip the following if not logged in  <-------------------------
+            //if (!thisB.browser.loginState) return;
+
+            if (typeof browser.config.classInterceptList === 'undefined') {
+                browser.config.classInterceptList = {};
+            }
+            
+            // setInterval(function() {
+            //     if ($('div.popup-dialog div.feature-detail')[0]) {
+            //         thisB.insertFeatureDetail();
+            //     } 
+            // },2000);
+
+            // load toolmenu "Analyze menu"
+            require(["plugins/JBMenu/js/toolmenu"], function(toolmenu){
+                toolmenu.init(browser,thisB);
+            });        
+        });
+
+
+    },
+
+    /**
+     * Display blast dialog box
+     * @returns {undefined}
+     */
+    startBlast: function() {
+        console.log("startBlast()");
+        var thisB = this;
+        var browser = this.browser;
+        let bpSize = browser._highlight.end - browser._highlight.start;
+
+        if (browser.jbmenu.isOversized(bpSize))  return;
+
+        browser.getStore('refseqs', dojo.hitch(this,function( refSeqStore ) {
+            if( refSeqStore ) {
+                var hilite = browser._highlight;
+                refSeqStore.getReferenceSequence(
+                    hilite,
+                    dojo.hitch( this, function( seq ) {
+						let bpSize = hilite.end-hilite.start;
+                        //console.log('startBlast() found sequence',hilite,bpSize);
+                        require(["JBrowse/View/FASTA"], function(FASTA){
+                            var fasta = new FASTA();
+                            var fastaData = fasta.renderText(hilite,seq);
+                            //console.log('FASTA',fastaData);
+                            //delete fasta;
+                            browser.jbmenuDialog(fastaData,bpSize);
+                        });                                
+
+                    })
+                );
+            }
+        }));             
+    },
+
+    // display blast dialog
+    Browser_jbmenuDialog: function (region,bpSize) {
+        var regionB = region;
+        var thisB = this;
+        var comboData = [];
+
+        //console.log("plugin",this);
+        this.jbmenu.getWorkflows(function(workflows){
+
+            if (workflows.length==0) {
+                alert("no workflows found");
+                return;
+            }
+            
+            var stateStore = new Memory({
+                data: comboData
+            });
+            
+            function destroyBlastDialog() {
+                dialog.destroyRecursive();
+            };
+            var dialog = new Dialog({ 
+                title: 'Process BLAST',
+                onHide: function() {
+                    destroyBlastDialog();
+                }
+            });
+            
+            dojo.create('span', {
+                innerHTML: 'Workflow '
+            }, dialog.containerNode);
+            
+            dojo.create('button', {
+                id: 'blast-workflow-select'
+            }, dialog.containerNode);
+            
+            for(var i in workflows) {
+                console.log("workflow",workflows[i]);
+                if (!workflows[i].deleted) {
+                    comboData.push({'name': workflows[i].name, 'id':workflows[i].id});
+                }
+            }
+
+            var comboBox = new ComboBox({
+                id: "workflow-combo",
+                name: "workflow",
+                value: comboData[0].name,
+                store: stateStore,
+                searchAttr: "name"
+            }, "blast-workflow-select").startup();            
+            
+            dojo.create('div', {
+                id: 'blast-box',
+                style: {'margin-top': '20px'},
+                innerHTML: 'This will process a BLAST search against the selected database.<br/><button id="submit-btn" type="button">Submit</button> <button id="cancel-btn" type="button">Cancel</button>'
+            }, dialog.containerNode);
+
+            var submitBtn = new Button({
+                label: "Submit",
+                onClick: function(){
+                    
+                    // get selected workflow
+                    var selStr = dijit.byId('workflow-combo').get('value');
+                    for(var x in comboData) {
+                        if (comboData[x].name == selStr) {
+                            var selWorkflow = comboData[x].id;
+                            console.log('Selected workflow',selWorkflow,comboData[x].name);
+                        }
+                    }
+                    console.log('Selected workflow',selWorkflow);
+                    
+                    var postData = {
+                          service: "jblast",
+                          dataset: thisB.config.dataRoot,
+                          region: regionB,
+                          workflow: selWorkflow
+                      };
+                    $.post( "/job/submit", postData , function( result ) {
+
+                        // open the job queue panel
+                        $('#extruderRight').openMbExtruder(true);$('#extruderRight').openPanel();
+                        
+                        // show confirm submit box
+                        var confirmBox = new Dialog({ title: 'Confirmation' });
+                        dojo.create('div', {
+                            id: 'confirm-btn',
+                            innerHTML: 'BLAST submitted...'
+                        }, confirmBox.containerNode );
+                        confirmBox.show();
+
+                        // close confirm box
+                        setTimeout(function(){ confirmBox.destroyRecursive();  }, 2000);
+
+                    }, "json")
+                    .fail(function(jqxhr, textStatus, errorThrown) {
+                        alert( "Job submit failed: "+jqxhr.responseText+" ("+jqxhr.status+"-"+jqxhr.statusText+")" );
+                    });
+
+                    destroyBlastDialog();
+                }
+            }, "submit-btn").startup();
+            var cancelBtn = new Button({
+                label: "Cancel",
+                onClick: function(){
+                    destroyBlastDialog();
+                }
+            }, "cancel-btn").startup();
+            
+			console.log("query region: ",regionB);
+			
+            if (dialog) dialog.show();
+
+        });
+    },
+
+    /**
+     * get galaxy workflows (using jbrowse api)
+     * @param {type} cb - cb(workflows]]
+     * @returns {getWorkflows}
+     */
+    getWorkflows(cb) {
+        let browser = this.browser;
+        
+        let xhr = $.get( "/service/exec/get_workflows?dataset="+browser.config.dataRoot, function( data ) {
+            console.log("get workflows result", data);
+            cb(data);
+        }).fail(function(jqxhr, textStatus, errorThrown) {
+            alert('get_workflows failed',textStatus);
+        });;
+    }
+
+});
+});
+
+
---
+++ plugins/JBMenu/js/queryDialog.js
@@ -0,0 +1,208 @@
+define([
+    'dojo/_base/declare',
+    'dojo/dom-construct',
+    'dojo/aspect',
+    'dijit/focus',
+    'dijit/Dialog',
+    'dijit/form/Button',
+    'dijit/form/RadioButton',
+    'dijit/form/CheckBox',
+    'dijit/form/TextBox',
+    'dijit/form/Textarea',
+    "dojo/store/Memory",
+    "dijit/form/ComboBox",
+    'JBrowse/View/Dialog/WithActionBar'
+],
+function(
+    declare,
+    dom,
+    aspect,
+    focus,
+    Dialog,
+    dButton,
+    dRButton,
+    dCheckBox,
+    dTextBox,
+    dTextArea,
+    Memory,
+    ComboBox,
+    ActionBarDialog
+) {
+
+return declare( ActionBarDialog, {
+
+constructor: function(args) {
+    this.plugin = args.plugin;
+    this.browser = args.browser;
+    this.workflows = args.workflows;
+    var thisB = this;
+
+    aspect.after( this, 'hide', function() {
+          focus.curNode && focus.curNode.blur();
+          setTimeout( function() { thisB.destroyRecursive(); }, 500 );
+    });
+},
+
+_dialogContent: function () {
+    
+    let workflows =this.workflows;
+    let wfStr = "";
+    for(var i in workflows) {
+        if (!workflows[i].deleted) {
+            wfStr += "<option value='"+workflows[i].id+"'>"+workflows[i].name+"</option>";
+        }
+    }
+    let container = dom.create('div', { className: 'search-dialog' } );
+
+    let workflowCombo = dom.create('div', {
+        id: 'workflow-div',
+        className: 'section',
+        innerHTML:
+            '<span classs="header">Select Workflow</span><br />'+ 
+            '<select id="workflow-combo" name="workflow">'+wfStr+'</select>'
+
+    },container);
+
+    let cfg = this.browser.config;
+    
+    //console.log("demo",cfg);
+    // if demo.blastButtons section is defined in trackList, display the buttons.
+    if (cfg.demo && cfg.demo.blastButtons && cfg.demo.blastButtons[0]) {
+        let blastButtons = dom.create('div', {
+            id: 'blastButtons',
+            style: {border: "1px solid lightgrey",padding: "5px"}
+        },container);
+
+        let btn = cfg.demo.blastButtons;
+
+        for(let i = 0;i < btn.length;i++) {
+            new dButton({
+                label: btn[i].button,
+                title: btn[i].description,
+                onClick: function() {
+                    console.log("click demo",btn[i].sequence.join(""));
+                    $('#sequence-text').val(btn[i].sequence.join(""));
+                }
+            })
+            .placeAt( blastButtons );
+        }
+    }
+
+
+    // Render textarea box
+    var searchBoxDiv = dom.create('div', {
+        className: "section",
+        innerHTML:
+            '<span classs="header">Input sequence to submit</span><br />'+ 
+            '<textarea id="sequence-text" class="seq-text" />'
+    }, container );
+
+    /*
+    function makeRadio( args, parent ) {
+        var label = dom.create('label', {}, parent );
+        var radio = new dRButton( args ).placeAt( label );
+        dom.create('span', { innerHTML: args.label }, label );
+        return radio;
+    }
+    
+    makeRadio( { name: 'translate', value: 'no', label: 'DNA', checked: true }, translateDiv );
+    content.translate = makeRadio( { name: 'translate', value: 'yes', label: 'AA' }, translateDiv );
+    */
+
+
+    return container;
+},
+
+_getSearchParams: function() {
+    var content = this.content;
+    //console.log("dialog result",$('.search-dialog #workflow-combo').find('option:selected').val(),$('.search-dialog #sequence-text').val());
+    return {
+        workflow: $('.search-dialog #workflow-combo').find('option:selected').val(),
+        sequence: $('.search-dialog #sequence-text').val()
+    };
+},
+
+_fillActionBar: function ( actionBar ) {
+    let thisB = this;
+    let browser = this.browser;
+
+    new dButton({
+            label: 'Submit',
+            //iconClass: 'dijitIconBookmark',
+            onClick: function() {
+                var searchParams = thisB._getSearchParams();
+                thisB.callback( searchParams );
+                thisB.hide();
+
+                // check if query size too big
+                let bpSize = thisB.countSequence(searchParams.sequence);
+                if (browser.jbmenu.isOversized(bpSize)) return;
+
+                var postData = {
+                    service: "jblast",
+                    dataset: browser.config.dataRoot,
+                    region: searchParams.sequence,
+                    workflow: searchParams.workflow,
+                    refseq:Object.keys(browser.allRefs)[0],
+                    unmappedSeq: true
+                };
+                console.log("post data",postData);
+                $.post( "/job/submit", postData , function( result ) {
+
+                    // open job queue panel
+                    $('#extruderRight').openMbExtruder(true);$('#extruderRight').openPanel();
+
+                    // show confirm submit box
+                    var confirmBox = new Dialog({ title: 'Confirmation' });
+                    dojo.create('div', {
+                        id: 'confirm-btn',
+                        innerHTML: 'submitted...'
+                    }, confirmBox.containerNode );
+                    confirmBox.show();
+
+                    // close confirm box
+                    setTimeout(function(){ confirmBox.destroyRecursive(); }, 2000);
+
+                }, "json")
+                .fail(function(jqxhr, textStatus, errorThrown) {
+                    alert( "Job submit failed: "+jqxhr.responseText+" ("+jqxhr.status+"-"+jqxhr.statusText+")" );
+                });
+            }
+        })
+        .placeAt( actionBar );
+    new dButton({
+            label: 'Cancel',
+            //iconClass: 'dijitIconDelete',
+            onClick: function() {
+                thisB.callback( false );
+                thisB.hide();
+            }
+        })
+        .placeAt( actionBar );
+},
+
+show: function ( callback ) {
+    this.callback = callback || function() {};
+    this.set( 'title', "BLAST a DNA sequence");
+    this.set( 'content', this._dialogContent() );
+    this.inherited( arguments );
+    focus.focus( this.closeButtonNode );
+},
+
+countSequence(seq) {
+    let lines = seq.split("\n");
+    let count = 0;
+
+    for(let i=0;i<lines.length;i++) {
+        console.log(i,lines[i]);
+        if (lines[i].charAt(0) !== ">") {
+            count += lines[i].length;
+        }
+    }
+   
+    return count;
+}
+
+
+});
+});
