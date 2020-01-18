2eead271abe0a614794fe404cd94d50d666166b4
enuggetry <ericiam@berkeley.edu>
2020-01-18 08:49:36 -0800

    add spawn process

--- api/services/workflowService.js
+++ api/services/workflowService.js
@@ -14,9 +14,13 @@
     };
 
 */
-/* istanbul ignore file */
-var fs = require("fs-extra");
-var approot = require("app-root-path");
+const path = require('path');
+const fs = require("fs-extra");
+const resolvePath = require('resolve-path');
+const approot = require("app-root-path");
+const shelljs = require('shelljs'); 
+const _ = require('lodash');
+const { exec, spawn } = require('child_process');
 
 module.exports = {
 
@@ -64,9 +68,9 @@ module.exports = {
         wflist = [];
         
         fs.readdirSync(wfpath).forEach(function(file) {
-            if (file.indexOf('.workflow.') !== -1) {
+            if (file.indexOf('.wf.') !== -1) {
                 
-                var name = file.split('.workflow.');
+                var name = file.split('.wf.');
                 
                 wflist.push( {
                    id: file,
@@ -102,18 +106,40 @@ module.exports = {
     // this is called by the job execution engine to begin processing
     beginProcessing(kJob) {
         let thisb = this;
+        let g = sails.config.globals.jbrowse;
+
+        let wf = approot+'/workflows/'+kJob.data.workflow;
+        //var outPath = g.jbrowsePath + kJob.data.dataset + '/' + g.jblast.blastResultPath;
+
         let nothingName = "sample nothing ";
         
         kJob.data.count = 10;   // 10 seconds of nothing
-       
-        let f1 = setInterval(function() {
-            if (kJob.data.count===0) {
-                clearInterval(f1);
-                thisb._postProcess(kJob);
-            }
-            kJob.data.name = nothingName+kJob.data.count--;
-            kJob.update(function() {});
-        },1000);
+
+        sails.log('>>> Executing workflow',wf);
+        const bat = spawn(wf);
+
+        bat.stdout.on('data', (data) => {
+          console.log('+'+data.toString());
+        });
+        bat.stderr.on('data', (data) => {
+          console.error('+'+data.toString());
+          kJob.update(function() {});
+        });
+        bat.on('exit', (code) => {
+          console.log(`+Child exited with code ${code}`);
+          kJob.update(function() {});
+          thisb._postProcess(kJob);
+        });        
+
+
+        // let f1 = setInterval(function() {
+        //     if (kJob.data.count===0) {
+        //         clearInterval(f1);
+        //         thisb._postProcess(kJob);
+        //     }
+        //     kJob.data.name = nothingName+kJob.data.count--;
+        //     kJob.update(function() {});
+        // },1000);
     },
 
     //  (not required)
@@ -125,6 +151,83 @@ module.exports = {
         this.postDoNothing(kJob,function(newTrackJson) {
             postAction.addToTrackList(kJob,newTrackJson);
         });
+    },
+    _runWorkflow: function(kJob) {
+
+        var thisb = this;
+        var g = sails.config.globals.jbrowse;
+
+        var wf = appPath+'/workflows/'+kJob.data.workflow;
+        var outPath = g.jbrowsePath + kJob.data.dataset + '/' + g.jblast.blastResultPath;
+
+        sails.log('>>> Executing workflow',wf);
+        
+        let optionFile = appPath + kJob.data.blastOptionFile;
+
+        let cmd = 'node '+ wf + ' --in '+kJob.data.blastData.blastSeq + ' --ext blastxml --out '+outPath+' --profile '+optionFile;
+        sails.log.debug("cmd",cmd);
+
+        var child = shelljs.exec(cmd,{async:true},
+            function(code, stdout, stderr) {
+                // istanbul ignore next
+                if (code !== 0) {    // completed in error
+                    console.log('Script Exit code:', code, typeof code);
+                    console.log('Script Program stderr:', stderr);                
+                }
+                console.log('Script output [[[[', stdout,']]]]');
+
+                // if no workflowResults terminate
+                // istanbul ignore next
+                if (stdout.indexOf("workflowResults:") === -1) {
+                    sails.log.error("failed to find workflow results");
+                    return kJob.kDoneFn(Error('workflow failed -', stdout));
+                }
+                
+                // extract workflow results
+                var results = extractjson(stdout);
+                if (results===false)    return kJob.kDoneFn(Error('workflow failed'));
+
+                // the script may generate any number of files so we rely on the script results to tell us the generated file
+                
+                kJob.data.workflowResult = results;
+                kJob.data.blastData.outputs = { blastxml: kJob.id+"_" + Date.now() };
+                kJob.update(function() {});
+                
+                // rename the generated file to be the asset id 
+                // this is a clunky should be improved.
+                //console.log(">>>>> renaming file asset",results.out);
+                renamefile(results.out);
+                
+                // start post processing.
+                thisb._postProcess(kJob);
+                
+                // extract the json from the result string
+                function extractjson(str) {
+                    let results = null;
+                    try {
+                        let jstr = str.split("workflowResults:")[1];
+                        console.log(">>>>> workflowresults",jstr);
+                        results = JSON.parse(jstr);
+                    }
+                    catch(err) {
+                        sails.log.error("extractjson() failed", err);
+                        return false;   
+                    }
+                    return results;
+                }
+                // rename to file asset name
+                function renamefile(theFile) {
+                    var dir = path.dirname(theFile);
+                    var ext = path.extname(theFile);
+                    var assetid = kJob.data.blastData.outputs.blastxml;
+                    var cmd = "mv "+theFile+" "+dir+"/"+assetid+ext;
+                    sails.log(cmd);
+                    shelljs.exec(cmd);
+                }
+            }
+        );
+        //sails.log('>>> Workflow complete');
+        
     },
 
     //  (not required)
---
+++ workflows/test1.wf.sh
@@ -0,0 +1,20 @@
+sleep 1s
+echo "1 second"
+sleep 1s
+echo "2 seconds"
+sleep 1s
+echo "3 seconds"
+sleep 1s
+echo "4 seconds"
+sleep 1s
+echo "5 seconds"
+sleep 1s
+echo "6 seconds"
+sleep 1s
+echo "7 seconds"
+sleep 1s
+echo "8 seconds"
+sleep 1s
+echo "9 seconds"
+sleep 1s
+echo "10 seconds"
