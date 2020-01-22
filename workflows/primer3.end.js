f650205711bc52f12e73e459b281ed76656941de
enuggetry <ericiam@berkeley.edu>
2020-01-22 14:21:07 -0800

    add global & cleanup left/right attribs (#122)

--- workflows/primer3.end.js
+++ workflows/primer3.end.js
@@ -59,21 +59,32 @@ try {
         let items = parser.items('');
         for(let j in items) {
             console.log(items[j][0],'=',items[j][1]);
-            if (items[j][0].includes(prePair)) {
-                grpPair += ';'+truncPre(items[j][0],prePair)+'='+items[j][1];
+            //if (items[j][0].includes(prePair)) {
+            //    grpPair += ';'+truncPre(items[j][0],prePair)+'='+items[j][1];
+            //}
+            if (items[j][0].includes('PRIMER_LEFT')) {
+                if (items[j][0].includes(preLeft))              grpLeft += ';'+truncPre(items[j][0],'PRIMER')+'='+items[j][1];
+                else if (items[j][0].indexOf('_',12) > -1)      ; 
+                else if (items[j][0].length <= 14)              ; 
+                else                                            grpLeft += ';'+truncPre(items[j][0],'PRIMER')+'='+items[j][1];
             }
-            if (items[j][0].includes(preLeft)) { 
-                grpLeft += ';'+truncPre(items[j][0],preLeft)+'='+items[j][1];
-            }
-            if (items[j][0].includes(preRight)) { 
-                grpRight += ';'+truncPre(items[j][0],preRight)+'='+items[j][1];
+            else if (items[j][0].includes('PRIMER_RIGHT')) {
+                if (items[j][0].includes(preRight))             grpRight += ';'+truncPre(items[j][0],'PRIMER')+'='+items[j][1];
+                else if (items[j][0].indexOf('_',12) > -1)      ; 
+                else if (items[j][0].length <= 14)              ; 
+                else                                            grpRight += ';'+truncPre(items[j][0],'PRIMER')+'='+items[j][1];
+            } 
+            else if (items[j][0] === 'SEQUENCE_TEMPLATE');
+            else {
+                grpPair += ';'+truncPre(items[j][0],'PRIMER')+'='+items[j][1];
             }
         }
     
-        console.log('>> grpPair',grpPair);
-        console.log('>> grpLeft',grpLeft);
-        console.log('>> grpRight',grpRight);
-
+        //console.log('>> grpPair',grpPair);
+        //console.log('>> grpLeft',grpLeft);
+        //console.log('>> grpRight',grpRight);
+        
+        // global and pair attributes
         gff +=  seq.seq + '\t'
                 + "primer3" + '\t'
                 + "primer" + '\t'
